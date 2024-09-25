93eb5f178199bc0e741440aeec47e97d66a78aaa
Operator <170519980488igo@gmail.com>
2024-09-25 22:18:08 +0300

    var 1.0.0

--- mymath.go
+++ mymath.go
@@ -7,14 +7,23 @@ func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
 
-func Abs(x float64) float64 {
-	return math.Abs(x)
-}
-
 func Max(x, y float64) float64 {
 	return math.Max(x, y)
 }
 
 func Yn(n int, x float64) float64 {
 	return math.Yn(n, x)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
 }
